a787ecd11467510f512beba03f8626cf79e6db14
Patrick Stein <Patrick.Stein@jinx.de>
2021-03-28 07:53:35 +0200

    Added missing Image(systemName:SFSymbol) function.

--- Sources/SFSymbolEnum/SFSymbolEnum.swift
+++ Sources/SFSymbolEnum/SFSymbolEnum.swift
@@ -8,10 +8,16 @@ public extension SFSymbol
     var image:Image { return Image(systemName:self.rawValue) }
 }
 
+@available(iOS 13.0, macOS 11.0, tvOS 13.0, watchOS 6.0, *)
+public func Image(systemName symbol:SFSymbol) -> Image
+{
+    return Image(systemName:symbol.name)
+}
 
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 public func Label(_ title:String, systemImage symbol:SFSymbol) -> Label<Text, Image>
 {
     return Label(title,systemImage:symbol.name)
 }
+
 
