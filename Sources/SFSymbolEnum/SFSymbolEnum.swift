5a77806ae84ae148f462fe70e5c71fd25ae8613c
Patrick Stein <Patrick.Stein@jinx.de>
2021-03-29 13:14:57 +0200

    added Label(_ title:String, symbol:SFSymbol) function

--- Sources/SFSymbolEnum/SFSymbolEnum.swift
+++ Sources/SFSymbolEnum/SFSymbolEnum.swift
@@ -20,9 +20,14 @@ public func Image(symbol:SFSymbol) -> Image
     return Image(systemName:symbol.name)
 }
 
-
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 public func Label(_ title:String, systemImage symbol:SFSymbol) -> Label<Text, Image>
+{
+    return Label(title,systemImage:symbol.name)
+}
+
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+public func Label(_ title:String, symbol:SFSymbol) -> Label<Text, Image>
 {
     return Label(title,systemImage:symbol.name)
 }
